255d20f0427f0e7dfb4e9fd9ba302ce64d0823bd
sskings <sergiosantanadosreis@hotmail.com>
2023-08-19 19:17:53 -0300

    Projeto iniciado, algumas imagens do projeto adicionado

---
+++ src/main/java/br/com/sskings/app/gestor/financeiro/AppGestorFinanceiro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.sskings.app.gestor.financeiro;
+
+/**
+ *
+ * @author s
+ */
+public class AppGestorFinanceiro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
